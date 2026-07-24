beb1ffff9b9797494a30aa352353628e457547b5
nzdqkh-creator <nzdqkh@outlook.com>
2026-07-24 15:35:19 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,82 +4,38 @@ const FILES = [
     "./",
     "./index.html",
     "./manifest.json",
-    "./icon-192.png",
+    "./icon.png",
     "./xlsx.full.min.js"
 ];
 
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES);
+        })
+    );
 
-self.addEventListener(
-    "install",
-    event => {
+    self.skipWaiting();
+});
 
-        event.waitUntil(
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(names => {
+            return Promise.all(
+                names
+                    .filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        })
+    );
 
-            caches
-            .open(CACHE_NAME)
-            .then(
-                cache =>
-                    cache.addAll(FILES)
-            )
+    self.clients.claim();
+});
 
-        );
-
-        self.skipWaiting();
-
-    }
-);
-
-
-self.addEventListener(
-    "activate",
-    event => {
-
-        event.waitUntil(
-
-            caches
-            .keys()
-            .then(names =>
-
-                Promise.all(
-
-                    names
-                    .filter(
-                        name =>
-                            name !== CACHE_NAME
-                    )
-                    .map(
-                        name =>
-                            caches.delete(name)
-                    )
-
-                )
-
-            )
-
-        );
-
-        self.clients.claim();
-
-    }
-);
-
-
-self.addEventListener(
-    "fetch",
-    event => {
-
-        event.respondWith(
-
-            caches
-            .match(event.request)
-            .then(cached =>
-
-                cached ||
-                fetch(event.request)
-
-            )
-
-        );
-
-    }
-);
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(cached => {
+            return cached || fetch(event.request);
+        })
+    );
+});
